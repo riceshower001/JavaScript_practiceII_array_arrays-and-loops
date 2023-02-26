311d040bffec52c9a210c2892db7f8a5bbee311a
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 03:09:35 +0900

    Add files via upload

---
+++ JavaScript_practiceII_array_arrays and loops.js
@@ -0,0 +1,8 @@
+// Let's output all the elements of an array using the for statement.
+
+const animals = ["dog", "cat", "sheep"];
+
+// Please use the for statement to output the values of an array in order.
+for(let i=0;i<3;i++){
+  console.log(animals[i]);
+}
